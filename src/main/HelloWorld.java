aa61667ca39b0f41e919e5e20f952a336618c095
Lee Mount <t-lemou@microsoft.com>
2016-07-19 15:08:29 -0400

    added YahooFinance jar + example

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,7 +1,18 @@
 package main;
+import java.io.IOException;
+import java.math.BigDecimal;
+
+import yahoofinance.*;
 
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("Hello world");
+		try {
+			Stock aapl = YahooFinance.get("AAPL");
+			BigDecimal price = aapl.getQuote().getPrice();
+			System.out.println("Current AAPL Price: " + price);
+			aapl.print();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
