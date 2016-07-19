c32c9a7f68997c7a0d7aaa563525b247098e16f4
Lee Mount <t-lemou@microsoft.com>
2016-07-19 15:26:48 -0400

    allowed access to javafx in properties

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -1,18 +1,43 @@
 package main;
 import java.io.IOException;
 import java.math.BigDecimal;
-
 import yahoofinance.*;
 
-public class HelloWorld {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application{
 	public static void main(String[] args){
-		try {
-			Stock aapl = YahooFinance.get("AAPL");
-			BigDecimal price = aapl.getQuote().getPrice();
-			System.out.println("Current AAPL Price: " + price);
-			aapl.print();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		launch(args);
 	}
+	
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Test UI Example");
+        Button btn = new Button();
+        btn.setText("Get current AAPL Price");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+            	try {
+					Stock aapl = YahooFinance.get("AAPL");
+	    			BigDecimal price = aapl.getQuote().getPrice();
+	                System.out.println("AAPL Price: " + price);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
