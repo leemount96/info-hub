81567f8e4b06a1468d505d8f75deb90b1465694b
Lee Mount <t-lemou@microsoft.com>
2016-07-20 15:25:17 -0400

    tested invalid ticker, returns null values

--- src/test/StockWorkerTest.java
+++ src/test/StockWorkerTest.java
@@ -1,5 +1,24 @@
 package test;
 
-public class StockWorkerTest {
+import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import org.junit.Test;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+public class StockWorkerTest {
+	
+	//Test illegal ticker name
+	@Test
+	public void testIllegalTicker(){
+		try {
+			Stock test = YahooFinance.get("ABCDEFG");
+			assertEquals(test.getQuote().getPrice(), null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
